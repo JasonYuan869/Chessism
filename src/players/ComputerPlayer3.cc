4d1a93fee8c52b641e0ddb27f48e84fbe0644878
Jason Yuan <jason.yuan869@gmail.com>
2023-07-30 02:37:33 -0400

    cleanup computerplayer3

--- src/players/ComputerPlayer3.cc
+++ src/players/ComputerPlayer3.cc
@@ -1,6 +1,7 @@
 #include "ComputerPlayer3.h"
 #include <algorithm>
 #include <cmath>
+#include "../pieces/KingPiece.h"
 
 using namespace std;
 
@@ -21,23 +22,32 @@ MoveResult ComputerPlayer3::makeMove(BoardState& board) {
         vector<pair<Move, int>> positions;
         vector<Piece*> pieces;
         if (isWhite) {
-            pieces = board.blackPieces;
-        } else {
             pieces = board.whitePieces;
+        } else {
+            pieces = board.blackPieces;
         }
 
+        // Same logic as ComputerPlayer2 so far (scoring based on captures only, not evading)
         for (Piece* p : pieces) {
+            if (!p->isAlive) {
+                continue;
+            }
             vector<Move> moves = p->validMoves; // get valid moves from piece
-            for (Move m : moves) {
+            for (const Move& m : moves) {
                 pair<int, int> to = m.getTo();
-                if (board.board[to.first][to.second] == nullptr) {
-                    pair<Move, int> thing{m, 0};
-                    positions.push_back(thing);
-                } else {
-                    int score = abs(board.board[to.first][to.second]->getValue());
-                    pair<Move, int> thing{m, score};
-                    positions.push_back(thing);
+                pair<Move, double> scoredMove{m, 0};
+                if (board.board[to.second][to.first] != nullptr) {
+                    double score = board.board[to.second][to.first]->getValue();
+                    scoredMove = {m, score};
                 }
+
+                // Check if the move puts the opponent in check
+                KingPiece* enemyKing = isWhite ? board.blackKing : board.whiteKing;
+                board.movePiece(m);
+                if (board.getAttacked(enemyKing->position_x, enemyKing->position_y,!isWhite)) {
+                    scoredMove.second += 0.5;
+                }
+                board.undo();
             }
         }
 
@@ -83,7 +93,7 @@ MoveResult ComputerPlayer3::makeMove(BoardState& board) {
     //     board.movePiece(move);
     //     return 1;
     // }
-    
+
     // return 0;
 }
 
